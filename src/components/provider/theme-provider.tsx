bdabfc3b0aecad76c9a55bbba7c45e1055820091
algnot <thanawat.tongla@gmail.com>
2025-04-10 10:46:49 +0700

    fix env

--- src/components/provider/theme-provider.tsx
+++ src/components/provider/theme-provider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <>
-      {(process.env.ENV ?? "local") != "production" && (
+      {(process.env.NEXT_PUBLIC_ENV ?? "local") != "production" && (
         <div className="fixed bottom-0 right-0 z-50 m-2 rounded-lg bg-gray-800 p-2 text-white text-sm">
           <p>Development Mode</p>
           <p>Env: {process.env.ENV}</p>
